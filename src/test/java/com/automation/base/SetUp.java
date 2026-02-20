6b874a1ea53ac8b25aa1b5d71458204aa352d03d
Nana Kwaku <nksarps@gmail.com>
2026-02-20 11:49:12 +0000

    refactor SetUp class to manage WebDriver lifecycle and initialize page objects before each test

--- src/test/java/com/automation/base/SetUp.java
+++ src/test/java/com/automation/base/SetUp.java
@@ -1,8 +1,22 @@
 package com.automation.base;
 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.automation.pages.AddCustomerPage;
+import com.automation.pages.BankManagerLoginPage;
+import com.automation.pages.CustomerDashboardPage;
+import com.automation.pages.CustomerLoginPage;
+import com.automation.pages.CustomersPage;
+import com.automation.pages.DepositPage;
+import com.automation.pages.LoginPage;
+import com.automation.pages.OpenAccountPage;
+import com.automation.pages.TransactionsPage;
+import com.automation.pages.WithdrawalPage;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -10,12 +24,25 @@ import io.github.cdimascio.dotenv.Dotenv;
  * Manages WebDriver lifecycle for test execution.
  */
 public class SetUp {
-    protected static WebDriver driver;
+    protected WebDriver driver;
+
+    // Creating instances of the page objects to be used across tests
+    protected LoginPage loginPage;
+    protected CustomerLoginPage customerLoginPage; 
+    protected WithdrawalPage withdrawalPage;
+    protected CustomerDashboardPage customerDashboardPage;
+    protected CustomersPage customersPage;
+    protected TransactionsPage transactionsPage;
+    protected AddCustomerPage addCustomerPage;
+    protected OpenAccountPage openAccountPage;
+    protected BankManagerLoginPage bankManagerLoginPage;
+    protected DepositPage depositPage;
 
     /**
      * Initializes the WebDriver and navigates to the application URL.
      */
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
 
         Dotenv dotenv = Dotenv.load();
@@ -30,19 +57,27 @@ public class SetUp {
 
         driver = new ChromeDriver(options);
         driver.get(applicationUrl);
+
+        // Initialize all page objects after driver is set up
+        loginPage = new LoginPage(driver);
+        customerLoginPage = new CustomerLoginPage(driver);
+        withdrawalPage = new WithdrawalPage(driver);
+        customerDashboardPage = new CustomerDashboardPage(driver);
+        customersPage = new CustomersPage(driver);
+        transactionsPage = new TransactionsPage(driver);
+        addCustomerPage = new AddCustomerPage(driver);
+        openAccountPage = new OpenAccountPage(driver);
+        bankManagerLoginPage = new BankManagerLoginPage(driver);
+        depositPage = new DepositPage(driver);
     }
 
-    /**
-     * Placeholder for per-test navigation setup.
-     */
-    public void beforeEachTest() {
-        // This method will open the application URL before each test case
-    }
+
 
     /**
      * Quits the WebDriver instance if it is active.
      */
-    public static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
